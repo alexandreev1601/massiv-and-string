2f682315cddf9eae2a3396ab1001cf24bd427c0f
alexandr andreev <alexandreev1601@gmail.com>
2024-04-07 21:56:46 +0500

    Задали массив символов

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Задайте массив сисволов (тип char []. Создайте сроку из символов этого массива)
+
+using System.Runtime.ExceptionServices;
+
+char[] array = new char[] { '1', '3', 'y', '3', 'h' };
+
+string str = "";
+for (int i = 0; i < array.Length; i++)
+{
+ str += array[i];
+}
+System.Console.WriteLine(str);
